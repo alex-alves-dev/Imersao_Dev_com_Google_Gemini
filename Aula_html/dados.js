e964f598248ce5deb1770b5f60f7e13538597ee9
Alex da Silva Alves <embaixadorbr@gmail.com>
2024-09-05 18:59:31 -0300

    Trabalhando com listas e objetos no javascript

---
+++ Aula_html/dados.js
@@ -0,0 +1,18 @@
+let dados = [ 
+ rayssa = {
+    titulo : "Rayssa Leal",
+    descricao: "Rayssa Leal, a fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos ela já coleciona títulos e inspira milhões de jovens.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+rebeca = {
+    titulo : "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+},
+beatriz = {
+    titulo : "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos jogos olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+}
+
+];
