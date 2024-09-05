3dc58e86228fbd12f2e644559bb42c0ad4227766
Alex da Silva Alves <embaixadorbr@gmail.com>
2024-09-05 19:46:26 -0300

    Implementando a funcionalidade de busca

--- Aula_html/app.js
+++ Aula_html/app.js
@@ -1,1 +1,24 @@
-console.log(dados[0].descricao);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+console.log(section)
+
+let resultados = ""
+
+for (let dado of dados) {
+    resultados += `
+    <div class="item-resultado">
+                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informacoes</a>
+            </div>
+`
+}
+
+section.innerHTML = resultados
+
+}
+// console.log(dados[0].descricao);
+
+
+
+
