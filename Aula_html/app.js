61145142184895d0f8adbc8f4ac378153929bcf8
Alex da Silva Alves <embaixadorb@gmail.com>
2024-09-06 13:01:31 -0300

    Finalizando o projeto com GitHub e Deploy

--- Aula_html/app.js
+++ Aula_html/app.js
@@ -2,19 +2,43 @@ function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
    
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nada foi encontrado, digite algo para pesquisar</p>" 
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
 
     // Inicializa uma string para armazenar os resultados formatados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre os dados e cria elementos HTML para cada resultado
     for (let dado of dados) {
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
             <div class="item-resultado">
                 <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href="${dado.link}" target="_blank">Mais informacoes</a>
             </div>
         `;
+        }
+        
+        
+        
+    }
+
+    if (!resultados) {
+        resultados = "<p> Nada foi encontrado</p>"
     }
 
     // Atribui os resultados formatados ao conteúdo da seção
--- Aula_html/dados.js
+++ Aula_html/dados.js
@@ -2,17 +2,20 @@ let dados = [
  rayssa = {
     titulo : "Rayssa Leal",
     descricao: "Rayssa Leal, a fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos ela já coleciona títulos e inspira milhões de jovens.",
-    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
 },
 rebeca = {
     titulo : "Rebeca Andrade",
     descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastica ginástica ouro"
 },
 beatriz = {
     titulo : "Beatriz Souza",
     descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos jogos olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka"
 }
 
 ];
